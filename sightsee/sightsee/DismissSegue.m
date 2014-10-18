9a0489a3f9915f9fb9ecd75bf824be9e3c558008
Ronald Ho <mail@ronaldho.me>
2014-10-18 08:42:00 -0700

    Added UI changes, JSON Parser

---
+++ sightsee/sightsee/DismissSegue.m
@@ -0,0 +1,13 @@
+//
+//  DismissSegue.m
+//  sightsee
+//
+//  Created by Ronald Ho on 2014-10-18.
+//  Copyright (c) 2014 Awesome Itinerary. All rights reserved.
+//
+
+#import "DismissSegue.h"
+
+@implementation DismissSegue
+
+@end
