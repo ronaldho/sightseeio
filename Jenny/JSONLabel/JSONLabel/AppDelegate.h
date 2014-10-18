74e10303ddce0142625a64bfb78017713ac06ffb
jennylia <jennylian100@gmail.com>
2014-10-18 06:34:08 -0700

    adding some good examples of how to parse JSON with objective C

---
+++ Jenny/JSONLabel/JSONLabel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JSONLabel
+//
+//  Created by Jenny Bunny on 2014-10-18.
+//  Copyright (c) 2014 Jenny Bunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Jenny/JSONLabel/JSONLabel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JSONLabel
+//
+//  Created by Jenny Bunny on 2014-10-18.
+//  Copyright (c) 2014 Jenny Bunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
