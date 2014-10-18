8deed3dc424971c501f9797743538768f9b4afac
Ronald Ho <mail@ronaldho.me>
2014-10-18 00:01:24 -0700

    Initial Push; some basic UI laid out

---
+++ sightsee/sightsee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sightsee
+//
+//  Created by Ronald Ho on 2014-10-17.
+//  Copyright (c) 2014 Awesome Itinerary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sightsee/sightsee/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sightsee
+//
+//  Created by Ronald Ho on 2014-10-17.
+//  Copyright (c) 2014 Awesome Itinerary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
