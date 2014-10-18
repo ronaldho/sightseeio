b074469fa36c3cfe6f5c2c5fc9caac7a2c8f517c
Matas <Matas@republicproject.com>
2014-10-18 12:10:20 -0700

    server response in .json now

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -5,5 +5,8 @@ class RequestController < ApplicationController
 		logger.debug "This is string: #{string}"
 		string = URI.encode(string)
 		@json = parser.generate_itinerary_json(string)
+		respond_to do |format|
+			format.json { render json: @json, status: :created}
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	root 'request#index'
+	get 'requests/requests.json', to: 'request#index', defaults: {format: 'json'}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
