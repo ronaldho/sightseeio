e39cb52b2d1664e428a61efbd2425519215eeb94
Ronald Ho <mail@ronaldho.me>
2014-10-20 21:29:10 -0700

    Displays ONE restaurant in Table Cell

--- sightsee/sightsee/main.m
+++ sightsee/sightsee/main.m
@@ -1,16 +1,1 @@
-//
-//  main.m
-//  sightsee
-//
-//  Created by Ronald Ho on 2014-10-17.
-//  Copyright (c) 2014 Awesome Itinerary. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-#import "AppDelegate.h"
-
-int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
-}
+////  main.m//  sightsee////  Created by Ronald Ho on 2014-10-17.//  Copyright (c) 2014 Awesome Itinerary. All rights reserved.//#import <UIKit/UIKit.h>#import "AppDelegate.h"int main(int argc, char * argv[]) {    @autoreleasepool {        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));    }}
